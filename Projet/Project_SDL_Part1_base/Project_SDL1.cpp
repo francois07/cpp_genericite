689d1054e48fb3b1755e372412d615fa586f4965
francois07 <francois.soulie@edu.esiee.fr>
2022-11-10 16:29:40 +0100

    Almost finished project

---
+++ Projet/Project_SDL_Part1_base/Project_SDL1.cpp
@@ -0,0 +1,195 @@
+﻿// SDL_Test.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Project_SDL1.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <numeric>
+#include <random>
+#include <string>
+
+#define WINDOW_HEIGHT 480
+#define WINDOW_WIDTH 640
+
+#define TEXTURE_SIZE 64
+
+void init()
+{
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO) < 0)
+    throw std::runtime_error("init():" + std::string(SDL_GetError()));
+
+  // Initialize PNG loading
+  int imgFlags = IMG_INIT_PNG;
+  if (!(IMG_Init(imgFlags) & imgFlags))
+    throw std::runtime_error("init(): SDL_image could not initialize! "
+                             "SDL_image Error: " +
+                             std::string(IMG_GetError()));
+}
+
+namespace
+{
+  // Defining a namespace without a name -> Anonymous workspace
+  // Its purpose is to indicate to the compiler that everything
+  // inside of it is UNIQUELY used within this source file.
+
+  SDL_Surface *load_surface_for(const std::string &path,
+                                SDL_Surface *window_surface_ptr)
+  {
+    // Helper function to load a png for a specific surface
+    // See SDL_ConvertSurface
+    auto path_char = path.c_str();
+    auto *surface = IMG_Load(path_char);
+
+    return surface;
+  }
+
+} // namespace
+
+animal::animal(const std::string &file_path, SDL_Surface *window_surface_ptr)
+    : image_ptr_{load_surface_for(file_path, window_surface_ptr)},
+      window_surface_ptr_{window_surface_ptr},
+      x_vel_{1},
+      y_vel_{1}
+{
+  this->x_pos_ = std::rand() % (WINDOW_WIDTH - TEXTURE_SIZE);
+  this->y_pos_ = std::rand() % (WINDOW_HEIGHT - TEXTURE_SIZE);
+  this->draw(window_surface_ptr);
+};
+// todo: The constructor has to load the sdl_surface that corresponds to the
+// texture
+animal::~animal()
+{
+  SDL_FreeSurface(image_ptr_);
+}; // todo: Use the destructor to release memory and "clean up
+   // behind you"
+
+void animal::draw(SDL_Surface *window_surface_ptr)
+{
+  SDL_Rect rect = SDL_Rect{this->x_pos_, this->y_pos_, TEXTURE_SIZE, TEXTURE_SIZE};
+  auto blitRes = SDL_BlitScaled(this->image_ptr_, NULL, window_surface_ptr, &rect);
+
+  if (blitRes != 0)
+  {
+    std::runtime_error("Couldn't draw texture on rectangle");
+  }
+}; // todo: Draw the animal on the screen <-> window_surface_ptr.
+   // Note that this function is not virtual, it does not depend
+   // on the static type of the instance
+
+sheep::sheep(const std::string &file_path, SDL_Surface *window_surface_ptr) : animal{file_path, window_surface_ptr}
+{
+}
+// Dtor
+sheep::~sheep()
+{
+}
+// implement functions that are purely virtual in base class
+void sheep::move()
+{
+  this->x_pos_ += 2 * this->x_vel_;
+  this->y_pos_ += 2 * this->y_vel_;
+
+  if (x_pos_ > WINDOW_WIDTH - TEXTURE_SIZE || x_pos_ < 0)
+  {
+    x_vel_ = -x_vel_;
+  }
+  if (y_pos_ > WINDOW_HEIGHT - TEXTURE_SIZE || y_pos_ < 0)
+  {
+    y_vel_ = -y_vel_;
+  }
+}; // todo: Animals move around, but in a different
+   // fashion depending on which type of animal
+
+wolf::wolf(const std::string &file_path, SDL_Surface *window_surface_ptr) : animal{file_path, window_surface_ptr}
+{
+}
+// Dtor
+wolf::~wolf() {}
+// implement functions that are purely virtual in base class
+void wolf::move()
+{
+  this->x_pos_ += 2 * this->x_vel_;
+  this->y_pos_ += 2 * this->y_vel_;
+
+  if (x_pos_ > WINDOW_WIDTH - TEXTURE_SIZE || x_pos_ < 0)
+  {
+    x_vel_ = -x_vel_;
+  }
+  if (y_pos_ > WINDOW_HEIGHT - TEXTURE_SIZE || y_pos_ < 0)
+  {
+    y_vel_ = -y_vel_;
+  }
+}; // todo: Animals move around, but in a different
+   // fashion depending on which type of animal
+
+/* Ground */
+ground::ground(SDL_Surface *window_surface_ptr) : window_surface_ptr_{window_surface_ptr}
+{
+}
+
+void ground::add_animal(std::shared_ptr<animal> a)
+{
+  this->animals_.push_back(a);
+  std::cout << this->animals_.size() << std::endl;
+}
+
+void ground::update()
+{
+  SDL_FillRect(this->window_surface_ptr_, NULL, 0x00FF00);
+  for (auto a : this->animals_)
+  {
+    a->move();
+    a->draw(window_surface_ptr_);
+  }
+}
+
+ground::~ground()
+{
+}
+
+/* Application */
+application::application(unsigned n_sheep, unsigned n_wolf)
+    : n_sheep_{n_sheep},
+      n_wolf_{n_wolf},
+      window_ptr_{SDL_CreateWindow("Projet C++", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN)},
+      window_surface_ptr_{SDL_GetWindowSurface(this->window_ptr_)},
+      ground_{this->window_surface_ptr_},
+      window_event_{}
+{
+  for (int i = 0; i < n_sheep_; i++)
+  {
+    std::shared_ptr<animal> a_ptr = std::make_shared<sheep>("../media/sheep.png", this->window_surface_ptr_);
+
+    this->ground_.add_animal(a_ptr);
+  }
+  for (int i = 0; i < n_wolf_; i++)
+  {
+    std::shared_ptr<animal> a_ptr = std::make_shared<wolf>("../media/wolf.png", this->window_surface_ptr_);
+
+    this->ground_.add_animal(a_ptr);
+  }
+}
+
+application::~application()
+{
+  SDL_DestroyWindow(this->window_ptr_);
+}
+
+int application::loop(unsigned period)
+{
+  int ticks = 0;
+  while (ticks < (period)*1000)
+  {
+    ticks = SDL_GetTicks();
+
+    this->ground_.update();
+    SDL_UpdateWindowSurface(this->window_ptr_);
+
+    SDL_Delay(10);
+  }
+
+  return 0;
+}
---
+++ Projet/Project_SDL_Part1_base/Project_SDL1.h
@@ -0,0 +1,140 @@
+﻿// SDL_Test.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <vector>
+
+// Defintions
+constexpr double frame_rate = 60.0; // refresh rate
+constexpr double frame_time = 1. / frame_rate;
+constexpr unsigned frame_width = 1400; // Width of window in pixel
+constexpr unsigned frame_height = 900; // Height of window in pixel
+// Minimal distance of animals to the border
+// of the screen
+constexpr unsigned frame_boundary = 100;
+
+// Helper function to initialize SDL
+void init();
+
+class animal
+{
+private:
+  SDL_Surface *window_surface_ptr_; // ptr to the surface on which we want the
+                                    // animal to be drawn, also non-owning
+  SDL_Surface *image_ptr_;          // The texture of the sheep (the loaded image), use
+                                    // load_surface_for
+  // todo: Attribute(s) to define its position
+protected:
+  int x_pos_;
+  int y_pos_;
+
+  int x_vel_;
+  int y_vel_;
+
+public:
+  animal(const std::string &file_path, SDL_Surface *window_surface_ptr);
+  // todo: The constructor has to load the sdl_surface that corresponds to the
+  // texture
+  ~animal(); // todo: Use the destructor to release memory and "clean up
+             // behind you"
+
+  void draw(SDL_Surface *window_surface_ptr); // todo: Draw the animal on the screen <-> window_surface_ptr.
+                                              // Note that this function is not virtual, it does not depend
+                                              // on the static type of the instance
+
+  virtual void move(){}; // todo: Animals move around, but in a different
+                         // fashion depending on which type of animal
+};
+
+// Insert here:
+// class sheep, derived from animal
+class sheep : public animal
+{
+public:
+  // todo
+  // Ctor
+  sheep(const std::string &file_path, SDL_Surface *window_surface_ptr);
+  // Dtor
+  ~sheep();
+  // implement functions that are purely virtual in base class
+  void move();
+};
+
+// Insert here:
+// class wolf, derived from animal
+// Use only sheep at first. Once the application works
+// for sheep you can add the wolves
+class wolf : public animal
+{
+public:
+  // todo
+  // Ctor
+  wolf(const std::string &file_path, SDL_Surface *window_surface_ptr);
+  // Dtor
+  ~wolf();
+  // implement functions that are purely virtual in base class
+  void move();
+
+  int get_target_x();
+  int get_target_y();
+
+  void set_target_y(int x);
+  void set_target_y(int y);
+
+private:
+  int target_x_;
+  int target_y_;
+};
+
+// The "ground" on which all the animals live (like the std::vector
+// in the zoo example).
+class ground
+{
+private:
+  // Attention, NON-OWNING ptr, again to the screen
+  SDL_Surface *window_surface_ptr_;
+
+  // Some attribute to store all the wolves and sheep
+  // here
+  std::vector<std::shared_ptr<animal>> animals_;
+
+public:
+  ground(SDL_Surface *window_surface_ptr);    // todo: Ctor
+  ~ground();                                  // todo: Dtor, again for clean up (if necessary)
+  void add_animal(std::shared_ptr<animal> a); // todo: Add an animal
+  void update();                              // todo: "refresh the screen": Move animals and draw them
+  // Possibly other methods, depends on your implementation
+};
+
+// The application class, which is in charge of generating the window
+class application
+{
+private:
+  // The following are OWNING ptrs
+  SDL_Window *window_ptr_;
+  SDL_Surface *window_surface_ptr_;
+  SDL_Event window_event_;
+
+  // Other attributes here, for example an instance of ground
+  unsigned n_sheep_;
+  unsigned n_wolf_;
+  ground ground_;
+
+public:
+  application(unsigned n_sheep, unsigned n_wolf); // Ctor
+  ~application();                                 // dtor
+
+  int loop(unsigned period); // main loop of the application.
+                             // this ensures that the screen is updated
+                             // at the correct rate.
+                             // See SDL_GetTicks() and SDL_Delay() to enforce a
+                             // duration the application should terminate after
+                             // 'period' seconds
+};
